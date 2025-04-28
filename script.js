b78fe767769da61ae47767e796f398caddb1f3ab
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-28 19:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const removeBtn = document.querySelector('input[type="button"]')
+
+function handleClick(){
+	const select = document.getElementById('colorSelect')
+	select.remove(select.selectedIndex)
+}
+removeBtn.addEventListener('click', handleClick)
